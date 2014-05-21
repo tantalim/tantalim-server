af3e497f3b9d1c3207167ea11cd82cc69dadfb90
Trevor Allred <trevorallred@gmail.com>
2014-05-21 00:32:30 -0700

    add some more dependencies for server

--- server.js
+++ server.js
@@ -2,11 +2,17 @@
 
 var express = require('express');
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var app = express();
 
 app.get('/', function (req, res) {
     res.send('Hello World');
 });
+
+var addClientFiles = function (clientDirectory) {
+    app.use(express.static(clientDirectory));
+};
 
 var start = function () {
     var server = app.listen(3000, function () {
@@ -14,5 +20,6 @@ var start = function () {
     });
 };
 
+exports.addClientFiles = addClientFiles;
 exports.start = start;
 exports.app = app;
