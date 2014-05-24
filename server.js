791fd99fb968e2a8df9643c4a32f3def55dd85a9
Trevor Allred <trevorallred@gmail.com>
2014-05-24 10:14:04 -0700

    fill out bulk of default web server

--- server.js
+++ server.js
@@ -1,25 +1,148 @@
 'use strict';
 
-var express = require('express');
+var _ = require('lodash'),
+    express = require('express'),
+    helpers = require('view-helpers');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var path = require('path');
+var rootPath = path.normalize(__dirname);
 
-var app = express();
+var config;
+var setup = function (custom) {
+    var defaultConfig = {
+        /**
+         * The absolute path to the root of this web server
+         * TODO - We might need to change this to the tantalum-server root
+         */
+        serverRoot: rootPath,
+        port: 3000,
+        environment: process.env.NODE_ENV || 'development',
+        sessionSecret: 'OVERRIDE_THIS_HASH_IN_YOUR_CONFIG',
+        sessionCollection: 'sessions'
+    };
+    config = _.extend(defaultConfig, custom || {});
+}
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-});
-
-var addClientFiles = function (clientDirectory) {
-    app.use(express.static(clientDirectory));
-};
-
+/**
+ *
+ * @returns Express App
+ */
 var start = function () {
-    var server = app.listen(3000, function () {
-        console.log('Tantalim Server running on port %d', server.address().port);
+    var app = express();
+
+    require('./app/routes/index')(app);
+
+    app.set('showStackError', true);
+
+    // Prettify HTML
+    app.locals.pretty = true;
+    // cache=memory or swig dies in NODE_ENV=production
+    app.locals.cache = 'memory';
+
+    // Should be placed before express.static
+    // To ensure that all assets and data are compressed (utilize bandwidth)
+    app.use(express.compress({
+        filter: function (req, res) {
+            return (/json|text|javascript|css/).test(res.getHeader('Content-Type'));
+        },
+        // Levels are specified in a range of 0 to 9, where-as 0 is
+        // no compression and 9 is best compression, but slowest
+        level: 9
+    }));
+
+    // Only use logger for development environment
+    if (process.env.NODE_ENV === 'development') {
+        app.use(express.logger('dev'));
+    }
+
+    // Create `ExpressHandlebars` instance with a default layout.
+    var exphbs = require('express3-handlebars');
+    var hbs = exphbs.create({
+//        defaultLayout: 'main',
+        helpers: require('./app/services/handlebarHelpers'),
+
+        extname: '.html',
+        // Uses multiple partials dirs, templates in "shared/templates/" are shared
+        // with the client-side of the app (see below).
+        partialsDir: [
+            'app/views/partials/'
+        ]
     });
+
+    // assign the template engine to .html files
+    app.engine('html', hbs.engine);
+    app.engine('js', hbs.engine);
+    // set .html as the default extension
+    app.set('view engine', 'html');
+
+    app.enable('view cache');
+    app.set('view options', {layout: false});
+
+
+    // Set views path, template engine and default layout
+    app.set('views', config.serverRoot + '/app/views');
+//    app.set('partials', config.serverRoot + '/app/views/partials');
+
+    // Enable jsonp
+    app.enable('jsonp callback');
+
+    app.configure(function () {
+        // The cookieParser should be above session
+        app.use(express.cookieParser());
+
+        // Request body parsing middleware should be above methodOverride
+        app.use(express.urlencoded());
+        app.use(express.json());
+        app.use(express.methodOverride());
+
+        // Express/Mongo session storage
+        app.use(express.cookieSession({
+            key: 'app.sess',
+            secret: config.sessionSecret
+        }));
+
+        // Dynamic helpers
+        app.use(helpers(config.app.name));
+
+        // Routes should be at the last
+        app.use(app.router);
+
+        // Setting the fav icon and static folder
+        app.use(express.favicon());
+        app.use(express.static(config.appRoot + '/public'));
+
+        // Assume "not found" in the error msgs is a 404. this is somewhat
+        // silly, but valid, you can do whatever you like, set properties,
+        // use instanceof etc.
+        app.use(function (err, req, res, next) {
+            // Treat as 404
+            if (~err.message.indexOf('not found')) return next();
+
+            // Log it
+            console.error(err.stack);
+
+            // Error page
+            res.status(500).render('500', {
+                error: err.stack
+            });
+        });
+
+        // Assume 404 since no middleware responded
+        app.use(function (req, res) {
+            res.status(404).render('404', {
+                url: req.originalUrl,
+                error: 'Not found'
+            });
+        });
+
+    });
+
+    var server = app.listen(config.port, function () {
+        console.log('Tantalim Server running %s on port:%d', config.environment, server.address().port);
+    });
+
+    return app;
 };
 
-exports.addClientFiles = addClientFiles;
+exports.setup = setup;
 exports.start = start;
-exports.app = app;
