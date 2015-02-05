412828e0f656e4e0dfdf33f51af36ab4050dc695
Trevor Allred <trevorallred@gmail.com>
2015-02-04 23:05:46 -0800

    fix passing controller tests

--- test/controllers/pageController.spec.js
+++ test/controllers/pageController.spec.js
@@ -7,63 +7,61 @@ var config = require('../config'),
     sinon = require('sinon'),
     BluebirdPromise = require('bluebird'),
     pageService = {},
-    modelService = {},
     chai = require('chai');
 
 chai.Should();
 chai.use(require('chai-as-promised'));
 
 describe('Page Controller', function () {
+    var controller, res,
+        req = {
+            pageName: 'TestPage'
+        };
 
-    describe('pageDefinition', function () {
-        var controller, res,
-            req = {
-                pageName: 'TestPage'
-            };
+    beforeEach(function () {
+        res = {
+            render: sinon.spy(),
+            sendfile: sinon.spy()
+        };
+        controller = proxyquire(configAppRoot + 'controllers/pageController', {
+            '../services/pageService': pageService
+        });
+    });
 
-        beforeEach(function () {
-            res = {
-                render: sinon.spy(),
-                sendfile: sinon.spy()
-            };
-            controller = proxyquire(configAppRoot + 'controllers/pageController', {
-                '../services/pageService': pageService,
-                '../services/modelService': modelService
-            });
+    it('should render error page', function (done) {
+        var err = 'something "correctly" went wrong';
+        pageService.getDefinition = BluebirdPromise.method(function () {
+            throw Error(err);
         });
 
-        it('should render error page', function (done) {
-            var err = 'something "correctly" went wrong';
-            pageService.getDefinition = BluebirdPromise.method(function () {
-                throw Error(err);
-            });
+        controller.pageDefinition(req, res).should.be.fulfilled.then(function () {
+            res.render.calledWith('page/error.js', {
+                code: 'Error',
+                message: 'something "correctly" went wrong'
+            }).should.be.true;
+        }).should.notify(done);
+    });
 
-            controller.pageDefinition(req, res).should.be.fulfilled.then(function () {
-                res.render.calledWith('page/error.js', err).should.be.true;
-            }).should.notify(done);
-        });
-
-        it('should render json page', function (done) {
-            var pageDefinition = {
-                modelName: 'TestModel'
-            };
-            var modelDefinition = {
-                id: 'TestModel'
-            };
-            pageService.getDefinition = BluebirdPromise.method(function () {
+    it('should render json page', function (done) {
+        var pageDefinition = {
+            modelName: 'TestModel'
+        };
+        var modelDefinition = {
+            id: 'TestModel'
+        };
+        pageService.getDefinition = BluebirdPromise.method(function (artifact_type) {
+            if (artifact_type === 'page')
                 return pageDefinition;
-            });
-            modelService.getDefinition = BluebirdPromise.method(function () {
+            else
                 return modelDefinition;
-            });
-
-            controller.pageDefinition(req, res)
-                .should.be.fulfilled.then(function () {
-                    res.render.calledWith('page/pageDefinition.js', {
-                        page: pageDefinition,
-                        model: modelDefinition
-                    }).should.be.true;
-                }).should.notify(done);
         });
+
+        controller.pageDefinition(req, res)
+            .should.be.fulfilled.then(function () {
+                res.render.calledWith('page/pageDefinition.js', {
+                    page: pageDefinition,
+                    model: modelDefinition
+                }).should.be.true;
+            }).should.notify(done);
     });
 });
